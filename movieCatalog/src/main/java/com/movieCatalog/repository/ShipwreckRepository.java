753f3a25088f47b2cfc602aca1c5a8c9aefca85c
Bove <rdbv@gft.com>
2017-03-29 11:40:14 -0600

    Add DB functionality

---
+++ movieCatalog/src/main/java/com/movieCatalog/repository/ShipwreckRepository.java
@@ -0,0 +1,11 @@
+package com.movieCatalog.repository;
+
+import com.movieCatalog.model.Shipwreck;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by rdbv on 3/29/2017.
+ */
+public interface ShipwreckRepository extends JpaRepository <Shipwreck, Long>{
+
+}
